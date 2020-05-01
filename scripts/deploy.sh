5760b55aaae228f06d3e2d623bf8c737fa6d5ac5
Halil <cryptopickle@outlook.com>
2020-05-01 19:01:58 +0100

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,17 @@
-git diff-tree --no-commit-id --name-only -r $GITHUB_SHA |
-grep  -e ".*\.go$" -e ".*\.yml$" | sed 's:[^/]*$::' | sort | uniq |
-while read line ; do
-  if [ -d "../$line" ]; then
-    if [ -f "../$line/serverless.yml" ] ; then
-      echo $line
-      make deploy  -C "../$line"
-    fi
-  fi
-done
+#git diff-tree --no-commit-id --name-only -r $GITHUB_SHA |
+#grep  -e ".*\.go$" -e ".*\.yml$" | sed 's:[^/]*$::' | sort | uniq |
+#while read line ; do
+#  if [ -d "../$line" ]; then
+#    if [ -f "../$line/serverless.yml" ] ; then
+#      echo $line
+#      make deploy  -C "../$line"
+#    fi
+#  fi
+#done
 
-#git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | grep
+
+IS_COMMON_UPDATED=$(git diff-tree --no-commit-id --name-only -r 12d5d23 | grep "services/common" | wc -l)
+
+if [ $IS_COMMON_UPDATED -gt 0 ] ; then
+  echo "TRUE"
+fi
