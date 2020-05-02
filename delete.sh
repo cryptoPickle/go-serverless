f723e43a2e8f290c760f90f36be0344d3cf0ba6b
Halil <cryptopickle@outlook.com>
2020-05-03 00:39:10 +0100

    test

--- delete.sh
+++ delete.sh
@@ -1,3 +1,4 @@
-find . -type d -exec sh -c '[ -f "$0"/serverless.yml ]' '{}' \; -print | while read line; do
+find . -type d -maxdepth 2 -exec sh -c '[ -f "$0"/serverless.yml ]' '{}' \; -print | while read line; do
+  echo $line
   cd $line ; sls remove
 done;
