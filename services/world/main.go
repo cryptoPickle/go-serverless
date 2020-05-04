263d491197a18f357ec60824c72e2ec6e952f32b
Halil <cryptopickle@outlook.com>
2020-05-04 17:14:52 +0100

    partial build on commit test

--- services/world/main.go
+++ services/world/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Okay so your other function also executed successfullys!",
+		"message": "Okay so your other function also executed successfully!",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
